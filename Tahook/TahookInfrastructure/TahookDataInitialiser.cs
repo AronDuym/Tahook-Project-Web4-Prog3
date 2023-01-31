b7de1151e8c1bc6ae14f2d2296d8a803d7c7f161
AronDuym <aron.duym@student.hogent.be>
2023-01-31 11:20:48 +0100

    Small Dummy Data

--- Tahook/TahookInfrastructure/TahookDataInitialiser.cs
+++ Tahook/TahookInfrastructure/TahookDataInitialiser.cs
@@ -4,6 +4,7 @@ using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tahook.DTO.Model;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace Tahook.Infrastructure
@@ -19,14 +20,14 @@ namespace Tahook.Infrastructure
 
         public void InitializeData()
         {
-            if (!_dbcontext.Quiz.Any())
+
+            if (!_dbcontext.GameTopic.Any())
             {
-                //TODO
-
-
-
-
-
+                for (int i = 0; i < 5; i++)
+                {
+                    GameTopic gameTopic = new GameTopic() { Description = "Wiskunde" + i, Quizzes = new List<Quiz>() }; 
+                    _dbcontext.GameTopic.Add(gameTopic);
+                }
 
 
                 _dbcontext.SaveChanges();
